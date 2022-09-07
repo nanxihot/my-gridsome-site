94d353930d342dea7df67dce419de12feaf8db9c
13276903830 <mrzc0303@163.com>
2022-09-07 15:24:49 +0800

    404页面

---
+++ src/pages/404.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>404页面,如果没有会生成默认的404页面</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"404Page",
+    data() {
+        return {
+
+        };
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+};
+</script>
+
+<style scoped lang="less">
+
+</style>

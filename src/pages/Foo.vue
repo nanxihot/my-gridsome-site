9796b7c6cef56a194f34dc8380824a602fa6689d
13276903830 <mrzc0303@163.com>
2022-09-07 12:41:28 +0800

    新建一个页面

---
+++ src/pages/Foo.vue
@@ -0,0 +1,30 @@
+<!-- 再pages中创建的页面会自动生成路由可以直接访问 -->
+<template>
+    <div>
+        <h1>Foo Page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'FooPage',
+    data() {
+        return {
+
+        };
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+};
+</script>
+
+<style scoped lang="less">
+
+</style>

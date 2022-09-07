bdc3263aaba5a52c41d63e8195e23a721a46122b
13276903830 <mrzc0303@163.com>
2022-09-07 15:41:30 +0800

    数据处理1

---
+++ src/pages/Posts1.vue
@@ -0,0 +1,32 @@
+<template>
+  <Layout>
+    <div>
+      <div>Posts 1</div>
+      <ul>
+        <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+      </ul>
+    </div>
+  </Layout>
+</template>
+
+<script>
+    import axios from 'axios'
+export default {
+  name: "name",
+  data() {
+    return {
+        posts:[],
+    };
+  },
+  async created() {
+    // 此处的数据只在客户端渲染，不会再服务端生成页面不利于seo优化
+    const {data} = await axios.get('http://jsonplaceholder.typicode.com/posts')
+    this.posts = data
+  },
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="less">
+</style>

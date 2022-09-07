2ac272c1c8f0d4b2086c982816cdb8604fd9d3f2
13276903830 <mrzc0303@163.com>
2022-09-07 15:04:08 +0800

    动态路由组件

---
+++ src/pages/user/[id].vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <!-- id即为文件名 -->
+        <h1>动态路由参数{{$route.params.id}}</h1>
+        <!-- 访问路径 user/id -->
+    </div>
+</template>
+
+<script>
+export default {
+    name:"UserPage",
+    data() {
+        return {
+
+        };
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+};
+</script>
+
+<style scoped lang="less">
+
+</style>

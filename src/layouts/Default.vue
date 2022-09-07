e394af91539b627867935f613086df18f4612724
13276903830 <mrzc0303@163.com>
2022-09-07 17:00:17 +0800

    数据预取 完成

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -8,6 +8,7 @@
         <g-link class="nav__link" to="/">Home</g-link>
         <g-link class="nav__link" to="/about/">About</g-link>
         <g-link class="nav__link" to="/posts1/">Posts 1</g-link>
+        <g-link class="nav__link" to="/posts2/">Posts 2</g-link>
       </nav>
     </header>
     <slot/>
---
+++ src/pages/Posts2.vue
@@ -0,0 +1,43 @@
+<template>
+  <Layout>
+    <div>
+      <h1>Posts2</h1>
+      <ul>
+        <li v-for="edge in $page.posts.edges" :key="edge.node.id">
+            <!-- <router-link to="/">{{edge.node.title}}</router-link> -->
+            <g-link to="/">{{edge.node.title}}</g-link>
+        </li>
+      </ul>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query{
+   posts: allPost{
+    edges{
+      node{
+        id
+        title
+        content
+      }
+    }
+    }
+} 
+</page-query>
+
+
+<script>
+export default {
+  name: "Posts2Page",
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="less">
+</style>

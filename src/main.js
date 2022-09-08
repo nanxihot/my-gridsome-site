175b4fb94669dc276f95659aaeadd17bd71cc12a
13276903830 <mrzc0303@163.com>
2022-09-08 16:08:32 +0800

    博客网站基本页面

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
+// 这个文件中可以加载全局的css样式
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
+import "./assets/css/index.css"
+
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component

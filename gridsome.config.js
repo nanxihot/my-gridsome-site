cbe2f95dd87186c56b0e7b7a6baf83715e2735ed
13276903830 <mrzc0303@163.com>
2022-09-12 22:21:28 +0800

    配置strapi插件

--- gridsome.config.js
+++ gridsome.config.js
@@ -8,13 +8,28 @@ module.exports = {
   siteName: 'Gridsome',
   plugins: [
     {
-      use: '@gridsome/source-filesystem',
+      use: '@gridsome/source-filesystem', // 用于读取本地md文件,转换为html
       options: {
         typeName: 'BlogPost',
         path: './content/blog/**/*.md', // 本地文件的路径
         remark: {
           // remark options
         }
+      }
+    },
+    {
+      use: '@gridsome/source-strapi', // 使用strapi
+      options: {
+        apiURL: 'http://localhost:1337',// strapi接口地址
+        queryLimit: 1000, // Defaults to 100 最多查询多少条数据
+        contentTypes: ['post'], // 查询的数据类型
+        // singleTypes: ['impressum'], // 单个节点
+        // Possibility to login with a Strapi user,
+        // when content types are not publicly available (optional).
+        // loginData: { // 有些数据是受保护的 需要登录 
+          // identifier: '',// 登录的信息
+          // password: '' // 登录的信息
+        // }
       }
     }
   ]
